31b97ecace818a588d08a184b1ac4a59980a1fae
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2025-02-08 15:19:13 +0530

    frontend-init

--- frontend-admin/src/App.tsx
+++ frontend-admin/src/App.tsx
@@ -1,13 +1,19 @@
 import { Textarea } from "@/components/ui/textarea"
+import {Button} from "@/components/ui/button.tsx";
+import {useState} from "react";
 
 function App() {
+    const  [jobDescription,setJobDescription] = useState("");
   return (
-    <div className={"flex flex-col items-center justify-center h-dvh"}>
+    <div className={"flex flex-col items-center justify-center h-dvh gap-2"}>
         <h1 className={"text-2xl font-bold"}>Job Description</h1>
       <Textarea
-          className={"w-10/12"}
+          className={"w-10/12 h-32 resize-none"}
           placeholder="Type your message here."
+          value={jobDescription}
+            onChange={(e) => setJobDescription(e.target.value)}
       />
+    <Button className={"mt-4"}>Submit</Button>
     </div>
   );
 }
